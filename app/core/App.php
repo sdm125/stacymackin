1d6b24c5a7f202806ff5d5b77162b2a0c4373133
Stacy Mackin <stacy.mackin@gmail.com>
2017-04-09 15:55:41 -0400

    make better

--- app/core/App.php
+++ app/core/App.php
@@ -11,7 +11,7 @@ class App
   {
     $url = $this->parseUrl();
 
-    if(file_exists('../app/controllers/' . $url[0] . '.php')) {
+    if(file_exists('../app/controllers/' . ucwords($url[0]) . '.php')) {
       $this->controller = $url[0];
       unset($url[0]);
     }
