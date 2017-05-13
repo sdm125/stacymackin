b1b71cd64a680bcb6253ac4cadec6d28cdccf489
Stacy Mackin <stacy.mackin@gmail.com>
2017-05-13 15:40:58 -0400

    make better

--- app/views/contact.php
+++ app/views/contact.php
@@ -1,18 +1,29 @@
-<div class="container about">
+<div class="container contact">
   <header>
     <h2 class="slide-down-in">CONTACT</h2>
   </header>
+
+  <div class="row mt-5 slide-down-in">
+    <div class="col-6">
+      <a href="https://github.com/sdm125" target="_blank"><i class="fa fa-5x fa-github" aria-hidden="true"></i><br>Github</a>
+    </div>
+    <div class="col-6">
+      <a href="mailto:stacy.mackin@gmail.com"><i class="fa fa-5x fa-envelope" aria-hidden="true"></i><br>Email</a>
+    </div>
+  </div>
+
   <form id="contactForm" method="post" action="" class="slide-down-in">
     <fieldset>
       <div class="row">
         <?php $fields = array('First Name' => ['first','text'], 'Last Name' => ['last','text'], 'Email' => ['email','email']); ?>
         <?php foreach($fields as $key => $value): ?>
-          <div class="col">
+          <?php echo ($key === 'Email') ? '<div class="col-12 col-sm-12">' : '<div class="col-12 col-sm-6">'; ?>
             <label>
               <div class="title">
                 <p class="hide-labels"><?php echo $key ?></p>
               </div>
-              <input type=<?php echo '"' . $value[1] . '"'; ?> name=<?php echo '"' . $value[0] . '"'; ?> placeholder=<?php echo '"' . $key . '"'; ?>>
+              <input type=<?php echo '"' . $value[1] . '"'; ?> name=<?php echo '"' . $value[0]
+              . '"'; ?> placeholder=<?php echo '"' . $key . '"'; ?> autocomplete="off">
             </label>
           </div>
         <?php endforeach ?>
