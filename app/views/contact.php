bfbb60c77ee8ad2f45453fb9afda8a4714d1c796
Stacy Mackin <stacy.mackin@gmail.com>
2017-05-13 14:50:43 -0400

    make better

--- app/views/contact.php
+++ app/views/contact.php
@@ -1,18 +1,20 @@
-<header>
-  <h2 class="slide-down-in">CONTACT</h2>
-</header>
 <div class="container about">
+  <header>
+    <h2 class="slide-down-in">CONTACT</h2>
+  </header>
   <form id="contactForm" method="post" action="" class="slide-down-in">
     <fieldset>
       <div class="row">
         <?php $fields = array('First Name' => ['first','text'], 'Last Name' => ['last','text'], 'Email' => ['email','email']); ?>
         <?php foreach($fields as $key => $value): ?>
-          <label class="col">
-            <div class="title">
-              <p class="hide-labels"><?php echo $key ?></p>
-            </div>
-            <input type=<?php echo '"' . $value[1] . '"'; ?> name=<?php echo '"' . $value[0] . '"'; ?> placeholder=<?php echo '"' . $key . '"'; ?>>
-          </label>
+          <div class="col">
+            <label>
+              <div class="title">
+                <p class="hide-labels"><?php echo $key ?></p>
+              </div>
+              <input type=<?php echo '"' . $value[1] . '"'; ?> name=<?php echo '"' . $value[0] . '"'; ?> placeholder=<?php echo '"' . $key . '"'; ?>>
+            </label>
+          </div>
         <?php endforeach ?>
       </div>
     </fieldset>
