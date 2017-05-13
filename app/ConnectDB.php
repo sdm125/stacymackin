1eb00f9e0b86464c67f20f473511dc3cb05d1b1f
Stacy Mackin <stacy.mackin@gmail.com>
2017-05-13 14:01:25 -0400

    make better

--- app/ConnectDB.php
+++ app/ConnectDB.php
@@ -6,8 +6,8 @@ class ConnectDB
   public static function connect()
   {
     try {
-      //$handler = new PDO('mysql:host=jlg7sfncbhyvga14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306;dbname=lktl9exoshaftugk', 'wbj99krq561xsclh', 'cgv89uwl70ycwdhe');
-      $handler = new PDO('mysql:host=localhost;dbname=stacy-mackin', 'stacy', 'lucy');
+      $handler = new PDO('mysql:host=jlg7sfncbhyvga14.cbetxkdyhwsb.us-east-1.rds.amazonaws.com:3306;dbname=lktl9exoshaftugk', 'wbj99krq561xsclh', 'cgv89uwl70ycwdhe');
+      //$handler = new PDO('mysql:host=localhost;dbname=stacy-mackin', 'stacy', 'lucy');
       $handler->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
       return $handler;
     } catch(PDOException $e) {
